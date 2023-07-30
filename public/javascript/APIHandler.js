3f2e70c9ea80f4fa1ca7eaaa529f3a150e4cf9f5
Rafael Brigati <johndoe@example.comgit config --global user.name Rafael>
2023-07-30 11:45:18 +0200

    Lab in progress

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -4,22 +4,68 @@ class APIHandler {
   }
 
   getFullList () {
-
+    axios
+    .get(this.BASE_URL)
+    .then((allCharacters) => {
+      console.log(allCharacters.data);
+      return allCharacters.data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
   }
 
   getOneRegister () {
+    if (id.length < 1) {
+      return;
+    }
+    axios
+      .get(this.BASE_URL + "/" + id)
+      .then((character) => {
+        console.log(character.data);
+        let { id, name, occupation, weapon, cartoon } = character.data;
+        console.log(id, name, occupation, weapon, cartoon);
+
+        document.getElementById("name-field").innerText = name;
+        document.getElementById("occupation-field").innerText = occupation;
+        document.getElementById("cartoon-field").innerText = cartoon;
+        document.getElementById("weapon-field").innerHTML = weapon;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
 
   }
 
   createOneRegister () {
-
+    axios
+      .post(this.BASE_URL)
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   updateOneRegister () {
-
+    axios
+    .put(this.BASE_URL + "/" + id)
+    .then((data) => {})
+    .catch((error) => {
+      console.log(error);
+    });
   }
 
   deleteOneRegister () {
-
-  }
+    axios
+    .delete(this.BASE_URL + "/" + id)
+    .then((data) => {
+      console.log(`DELETED ITEM FROM DB ${data}`);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
+}
+
